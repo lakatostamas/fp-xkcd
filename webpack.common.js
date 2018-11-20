87bc7b9673e7f9c45a48686217e9c02da6487273
lakatostamas <mail.tamaslakatos@gmail.com>
2018-11-20 18:07:55 +0100

    chore(build): add basic webpack && babel config

---
+++ webpack.common.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    flashcards: path.join(__dirname, 'src', 'index.js'),
+  },
+
+  resolve: {
+    extensions: ['.js'],
+  },
+
+  target: 'web',
+
+  context: __dirname,
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        loader: 'babel-loader',
+        options: { cacheDirectory: true },
+      },
+    ],
+  },
+};
---
+++ webpack.dev.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  devtool: 'eval-source-map',
+
+  mode: 'development',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+});
