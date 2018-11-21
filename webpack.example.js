8b60fbf7b099b2c99b798347ad8e1681861ae0be
lakatostamas <mail.tamaslakatos@gmail.com>
2018-11-21 18:33:18 +0100

    chore(build): modify example page title

--- webpack.example.js
+++ webpack.example.js
@@ -17,7 +17,7 @@ module.exports = merge(dev, {
         <head>
           <meta charset="utf-8">
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-          <title>FlashCards</title>
+          <title>FP Flicker</title>
         </head>
         <body>
         </body>
