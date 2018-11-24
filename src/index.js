19a0eed0babda135ac14492cbec5f3a4f98f1d96
lakatostamas <mail.tamaslakatos@gmail.com>
2018-11-24 12:35:10 +0100

    style(app): fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
-import { compose, curry, map, prop } from 'ramda';
-import { task, of } from 'folktale/concurrency/task';
+import {
+  compose,
+  curry,
+  prop,
+} from 'ramda';
+import { task } from 'folktale/concurrency/task';
 import axios from 'axios';
 
 const Impure = {
   get: url => task(resolver => axios.get(url)
     .then(resolver.resolve)
-    .catch(resolver.reject)
-  ).run(),
-  setHtml: curry((sel, html) => document.querySelector(sel).innerHTML = html),
-  addEventListener: curry((sel, event, listener) => document.querySelector(sel).addEventListener(event, listener)),
-  trace: log => console.log(log),
+    .catch(resolver.reject)).run(),
+  setHtml: curry((sel, html) => {
+    document.querySelector(sel).innerHTML = html;
+  }),
+  addEventListener: curry(
+    (sel, event, listener) => document.querySelector(sel).addEventListener(event, listener),
+  ),
 };
 
 const apiUrl = 'https://xkcd.now.sh/';
 const buildImageTag = src => `<img src=${src} />`;
 const dataToImage = compose(buildImageTag, prop('img'), prop('data'));
 const renderImage = compose(Impure.setHtml('.result-container'), dataToImage);
-const renderErrorMessage = compose(Impure.setHtml('.result-container'), prop('message'))
+const renderErrorMessage = compose(Impure.setHtml('.result-container'), prop('message'));
 const transformTextToUrl = pattern => `${apiUrl}${pattern}`;
 const extractPattern = compose(prop('value'), prop('target'));
 
